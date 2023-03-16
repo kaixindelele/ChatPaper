407f81b0e3b659c21fe4a64fb6ea0474f270bee3
panhaoxuan <haoxuanpan0514@outlook.com>
2023-03-17 00:06:33 +0800

    fix typo

--- chat_paper.py
+++ chat_paper.py
@@ -168,7 +168,7 @@ class Reader:
             text = ''
             text += 'Title:' + paper.title
             text += 'Url:' + paper.url
-            text += 'Abstrat:' + paper.abs
+            text += 'Abstract:' + paper.abs
             text += 'Paper_info:' + paper.section_text_dict['paper_info']
             # intro
             text += list(paper.section_text_dict.values())[0]
